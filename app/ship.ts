b7e91df15964f693299d4d3462632804d023dbfd
David <petah.p@gmail.com>
2019-01-25 23:46:09 +1300

    Mouse control

--- app/ship.ts
+++ app/ship.ts
@@ -27,32 +27,37 @@ export default class Ship implements Entity{
 
         this.scene.input.on('pointerdown',  (pointer) => {
             const direction = GM.pointDirection(this.image.x, this.image.y, this.scene.cameras.main.worldView.x + pointer.x, this.scene.cameras.main.worldView.y + pointer.y);
-            const bullet = new Bullet(this.scene, this.image.x, this.image.y, direction);
-            this.scene.addEntity(bullet);
-            // console.log(this.scene.cameras.main.worldView.x + pointer.x, this.scene.cameras.main.worldView.y + pointer.y);
+            if (pointer.buttons == 1) {
+                this.direction = direction;
+                this.speed = this.maxSpeed;
+            }
+            if (pointer.buttons == 2) {
+                const bullet = new Bullet(this.scene, this.image.x, this.image.y, direction);
+                this.scene.addEntity(bullet);
+            }
         });
     }
 
     update() {
-        if (this.cursors.space.isDown) {
-            this.speed -= this.acceleration;
-        } else if (this.cursors.right.isDown && this.cursors.down.isDown) {
-            GM.motionAdd(this, this.acceleration, 45);
-        } else if (this.cursors.left.isDown && this.cursors.down.isDown) {
-            GM.motionAdd(this, this.acceleration, 135);
-        } else if (this.cursors.left.isDown && this.cursors.up.isDown) {
-            GM.motionAdd(this, this.acceleration, 225);
-        } else if (this.cursors.right.isDown && this.cursors.up.isDown) {
-            GM.motionAdd(this, this.acceleration, 315);
-        } else if (this.cursors.right.isDown) {
-            GM.motionAdd(this, this.acceleration, 0);
-        } else if (this.cursors.down.isDown) {
-            GM.motionAdd(this, this.acceleration, 90);
-        } else if (this.cursors.left.isDown) {
-            GM.motionAdd(this, this.acceleration, 180);
-        } else if (this.cursors.up.isDown) {
-            GM.motionAdd(this, this.acceleration, 270);
-        }
+        // if (this.cursors.space.isDown) {
+        //     this.speed -= this.acceleration;
+        // } else if (this.cursors.right.isDown && this.cursors.down.isDown) {
+        //     GM.motionAdd(this, this.acceleration, 45);
+        // } else if (this.cursors.left.isDown && this.cursors.down.isDown) {
+        //     GM.motionAdd(this, this.acceleration, 135);
+        // } else if (this.cursors.left.isDown && this.cursors.up.isDown) {
+        //     GM.motionAdd(this, this.acceleration, 225);
+        // } else if (this.cursors.right.isDown && this.cursors.up.isDown) {
+        //     GM.motionAdd(this, this.acceleration, 315);
+        // } else if (this.cursors.right.isDown) {
+        //     GM.motionAdd(this, this.acceleration, 0);
+        // } else if (this.cursors.down.isDown) {
+        //     GM.motionAdd(this, this.acceleration, 90);
+        // } else if (this.cursors.left.isDown) {
+        //     GM.motionAdd(this, this.acceleration, 180);
+        // } else if (this.cursors.up.isDown) {
+        //     GM.motionAdd(this, this.acceleration, 270);
+        // }
         if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
         }
