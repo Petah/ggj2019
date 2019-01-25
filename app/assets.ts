ee336ac11da74bf1578a52253e912dc807cf3d3c
Joey Lee <slarinz@gmail.com>
2019-01-26 11:27:12 +1300

    Update planet sprite images

--- app/assets.ts
+++ app/assets.ts
@@ -12,20 +12,59 @@ export default class Assets {
         this.scene.load.image('bullet', 'bullet.png');
         this.scene.load.image('ship', 'ship.png');
 
-        this.scene.load.spritesheet('planet1', 'planet1-sprite.png', {
+        this.scene.load.spritesheet('planet-gaia', 'planet-gaia-sprite.png', {
             frameWidth: 72,
-            frameHeight: 72,
+            frameHeight: 69,
+        });
+        this.scene.load.spritesheet('planet-ice', 'planet-ice-sprite.png', {
+            frameWidth: 72,
+            frameHeight: 69,
+        });
+        this.scene.load.spritesheet('planet-tundra', 'planet-tundra-sprite.png', {
+            frameWidth: 72,
+            frameHeight: 69,
+        });
+        this.scene.load.spritesheet('planet-barren', 'planet-barren-sprite.png', {
+            frameWidth: 72,
+            frameHeight: 69,
         });
     }
 
     create() {
         this.scene.anims.create({
-            key: 'planet1-animation',
-            frames: this.scene.anims.generateFrameNumbers('planet1', {
+            key: 'planet-gaia-animation',
+            frames: this.scene.anims.generateFrameNumbers('planet-gaia', {
                 start: 0,
                 end: 18,
             }),
-            frameRate: 30,
+            frameRate: 10,
+            repeat: -1,
+        });
+        this.scene.anims.create({
+            key: 'planet-ice-animation',
+            frames: this.scene.anims.generateFrameNumbers('planet-ice', {
+                start: 0,
+                end: 18,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        this.scene.anims.create({
+            key: 'planet-tundra-animation',
+            frames: this.scene.anims.generateFrameNumbers('planet-tundra', {
+                start: 0,
+                end: 18,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        this.scene.anims.create({
+            key: 'planet-barren-animation',
+            frames: this.scene.anims.generateFrameNumbers('planet-barren', {
+                start: 0,
+                end: 18,
+            }),
+            frameRate: 10,
             repeat: -1,
         });
     }
