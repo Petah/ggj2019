dde0210f18058a9c37a9c9e848c76f2a5e43e31a
DanielsUserName <itsdaniellarmour@outlook.com>
2019-01-25 20:50:33 +1300

    Add base game events

---
+++ app/game_objects/game_events/gameEvent.ts
@@ -0,0 +1,21 @@
+class gameEvent {
+    title: string;
+    message: string;
+    imageLocation: string;
+    eventOptions: GameEventSelectableOption[];
+    actors: any[]; // JS pls
+
+    constructor(
+        title: string,
+        message: string,
+        imageLocation: string,
+        eventOptions: GameEventSelectableOption[],
+        actors: any[]
+    ) {
+        this.title = title;
+        this.message = message;
+        this.imageLocation = imageLocation;
+        this.eventOptions = eventOptions;
+        this.actors = actors;
+    }
+}
---
+++ app/game_objects/game_events/gameEventSelectableOptionAction.ts
@@ -0,0 +1,3 @@
+interface GameEventSelectableOptionAction {
+    (): void
+}
